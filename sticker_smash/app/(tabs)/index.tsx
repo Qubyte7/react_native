20bba962860fed81f73d6dcc99d60e7bfd3e9514
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2025-04-30 15:24:03 +0200

    using image -picker

--- sticker_smash/app/(tabs)/index.tsx
+++ sticker_smash/app/(tabs)/index.tsx
@@ -2,7 +2,8 @@ import { View } from "react-native";
 import { StyleSheet } from "react-native";
 import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
-
+import * as ImagePicker from 'expo-image-picker'
+import { useState } from "react";
 
 const PlaceholderImage =  require('@/assets/images/background-image.png');
 
@@ -41,13 +42,27 @@ const styles = StyleSheet.create({
 
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+  let pickImageAsync = async ()=>{
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes:['images'],
+      allowsEditing: true,
+      quality:1,
+    })
+    if(!result.canceled){
+      setSelectedImage(result.assets[0].uri)
+      console.log(result);
+    }else{
+      alert("You did not select any image");
+    }
+  }
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
+        <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button label="Choose a photo" theme="primary"/>
+        <Button label="Choose a photo" theme="primary" onPress={pickImageAsync}/>
         <Button label="Use this photo"/>
       </View>
     </View>
