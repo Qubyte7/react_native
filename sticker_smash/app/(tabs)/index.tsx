42e9679245dc944bcaf3a4050f54025618e38505
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2025-04-30 19:55:33 +0200

    screenshot and save [still some white screen issue]

--- sticker_smash/app/(tabs)/index.tsx
+++ sticker_smash/app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
 import * as ImagePicker from 'expo-image-picker'
-import { useState } from "react";
+import { useState,useRef } from "react";
 import IconButton from "@/components/IconButton";
 import CircleButton from "@/components/CircularButton";
 import EmojiPicker from "@/components/EmojPicker";
@@ -11,15 +11,23 @@ import { type ImageSource } from "expo-image";
 import EmojiList from "@/components/EmojiList";
 import EmojiSticker from "@/components/EmojiSticker";
 import {GestureHandlerRootView} from 'react-native-gesture-handler'
-
+import * as MediaLibrary from  'expo-media-library'
+import { captureRef } from "react-native-view-shot";
 
 const PlaceholderImage =  require('@/assets/images/background-image.png');
 
 export default function Index() {
+  const [status,requestPermission] = MediaLibrary.usePermissions();
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
   const [showOptions,setShowOptions] = useState<boolean>(false);
   const[isModalVisible,setIsModalVisible] = useState<boolean>(false);
   const [pickedEmoji,setPickedEmoji] = useState<ImageSource | undefined>(undefined);
+  const imageRef = useRef<View>(null);
+
+  if(!status==null){
+    requestPermission();
+  }
+
 
   let pickImageAsync = async ()=>{
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -46,16 +54,30 @@ export default function Index() {
     setIsModalVisible(false);
   };
 
-  const onSaveImageAsync = ()=>{
-
+  const onSaveImageAsync = async()=>{
+    try{
+      const localUri = await captureRef(imageRef,{
+        height: 440,
+        quality: 1,
+      });
+      await MediaLibrary.saveToLibraryAsync(localUri);
+      if(localUri){
+        alert("Image saved to gallery");
+    }
+  }catch(e){
+    console.log(e);
+    
   }
+
 
   return (
     <GestureHandlerRootView style={styles.container}>
     <View >
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
-        {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
+        <View ref={imageRef} collapsable={false}>
+          <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
+          {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
+        </View> 
       </View>
       {showOptions ? (
         <View>
@@ -80,7 +102,7 @@ export default function Index() {
     </GestureHandlerRootView>
   );
 }
-
+}
 
 const styles = StyleSheet.create({
   container:{
